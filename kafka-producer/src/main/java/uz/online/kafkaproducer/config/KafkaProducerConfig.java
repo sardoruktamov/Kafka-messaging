9823fd0a18cbcd29041e4746d05a9d8d6f1a0629
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2023-12-20 23:01:49 +0300

    Update Kafka-Producer

--- kafka-producer/src/main/java/uz/online/kafkaproducer/config/KafkaProducerConfig.java
+++ kafka-producer/src/main/java/uz/online/kafkaproducer/config/KafkaProducerConfig.java
@@ -9,10 +9,13 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.core.RoutingKafkaTemplate;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.ProducerListener;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import uz.online.kafkaproducer.config.data.ProducerConfigData;
@@ -52,9 +55,9 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public ProducerFactory<Object,Object> defaultKafkaProducerFactory() throws ClassNotFoundException {
+    public ProducerFactory<String,String> defaultKafkaProducerFactory() throws ClassNotFoundException {
         var defaultProducerFactory = new DefaultKafkaProducerFactory<Object,Object>(
-                producerConfigs(), () -> new JsonSerializer<>(), () -> new JsonSerializer<>());
+                producerConfigs(), () -> new StringSerializer(), () -> new StringSerializer());
         defaultProducerFactory.setProducerPerThread(true);
         return defaultProducerFactory;
     }
@@ -65,5 +68,17 @@ public class KafkaProducerConfig {
         var byteValueKafkaConfig = new HashMap<>(producerConfigs());
         byteValueKafkaConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
         return new DefaultKafkaProducerFactory<>(byteValueKafkaConfig);
+    }
+
+    @Bean
+    public ReplyingKafkaTemplate<Object,Object,String> replyingKafkaTemplate(ConcurrentMessageListenerContainer<Object,String> messageListenerContainer) throws ClassNotFoundException {
+        return new ReplyingKafkaTemplate<>(defaultKafkaProducerFactory(), messageListenerContainer);
+    }
+    @Bean
+    public ConcurrentMessageListenerContainer<Object,String> concurrentMessageListenerContainer(ConcurrentKafkaListenerContainerFactory<Object,String> listenerContainerFactory){
+        var concurentListenerContainer = listenerContainerFactory.createContainer("second");
+        concurentListenerContainer.getContainerProperties().setGroupId("kSecond");
+        concurentListenerContainer.setAutoStartup(false);
+        return concurentListenerContainer;
     }
 }
--- kafka-producer/src/main/java/uz/online/kafkaproducer/service/KafkaProducerService.java
+++ kafka-producer/src/main/java/uz/online/kafkaproducer/service/KafkaProducerService.java
@@ -1,15 +1,19 @@
 package uz.online.kafkaproducer.service;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.RoutingKafkaTemplate;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -21,6 +25,9 @@ public class KafkaProducerService {
 
     @Autowired
     private RoutingKafkaTemplate routingKafkaTemplate;
+
+    @Autowired
+    private ReplyingKafkaTemplate<Object,Object,String> replyingKafkaTemplate;
 
     public void send(String topic, String message){
         try {
@@ -40,6 +47,21 @@ public class KafkaProducerService {
                     .thenAccept(result -> log.info("Message was sent by routing to kafka topic: {} ", result.getRecordMetadata().topic()));
         } catch (Exception e) {
             log.error("Error while sending message by routing to {}, error: {}", topic,e.getMessage());
+        }
+    }
+
+    public void sendAndRecieve(String topic, Object value){
+        try {
+            ProducerRecord<Object,Object> record = new ProducerRecord<>("simple",value);
+            var response = replyingKafkaTemplate.sendAndReceive(record);
+            var sendresult = response.getSendFuture().get();
+            log.info("sent result: {}",sendresult.getRecordMetadata().topic());
+            response.thenAccept(r -> {
+                log.info("Reply message is received from {}, Response Data:{}",r.topic(),r.value());
+            });
+            var replyingValue = response.get();
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("Error while sending and receive object via replying kafka template: {}", e.getMessage());
         }
     }
 }
