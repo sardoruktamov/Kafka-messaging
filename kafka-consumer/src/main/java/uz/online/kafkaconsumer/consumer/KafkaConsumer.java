ac3fb6b7f8c5075ddbad12cc582dd8afa8efcc36
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2023-12-20 22:49:43 +0300

    Update KafkaConsumer.java

--- kafka-consumer/src/main/java/uz/online/kafkaconsumer/consumer/KafkaConsumer.java
+++ kafka-consumer/src/main/java/uz/online/kafkaconsumer/consumer/KafkaConsumer.java
@@ -1,14 +1,20 @@
 package uz.online.kafkaconsumer.consumer;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class KafkaConsumer {
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
 
     @KafkaListener(id = "simpleID",topics = "simple")
     public void simpleListener(String message){
+        kafkaTemplate.send("second", "Replying message from consumer");
         log.info("Message receive(Xabarni qabul qilish): {}", message);
     }}
